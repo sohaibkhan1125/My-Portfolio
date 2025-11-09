851eeebc9a8adb480e1ab377e6bd756a74c9850b
SohaibKhan1125 <alihassankhan1192@gmail.com>
2025-11-09 20:33:43 +0500

    solve issue

---
+++ app/types.d.ts
@@ -0,0 +1,21 @@
+// app/types.d.ts
+declare module "aos" {
+  interface AOSOptions {
+    offset?: number;
+    delay?: number;
+    duration?: number;
+    easing?: string;
+    once?: boolean;
+    mirror?: boolean;
+    anchorPlacement?: string;
+  }
+
+  interface AOS {
+    init(options?: AOSOptions): void;
+    refresh(): void;
+    refreshHard(): void;
+  }
+
+  const AOS: AOS;
+  export default AOS;
+}
